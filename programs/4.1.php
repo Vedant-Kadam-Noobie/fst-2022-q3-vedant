247efe1f712ddfe7871c31fa551b48294823d964
Vedant Kadam <91219492+Vedant-Kadam-Noobie@users.noreply.github.com>
2022-07-30 15:22:41 +0530

    Add files via upload

---
+++ programs/4.1.php
@@ -0,0 +1,23 @@
+<?php
+$servername ="localhost";
+$username ="root";
+$password ="123456";
+$dbName ="practise";
+
+$conn = new mysqli($servername, $username, $password, $dbName);
+
+$sql = "use practise";
+$result = $conn->query($sql);
+
+$sql1 = "SELECT * FROM data";
+$result = $conn->query($sql1);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "\n" . $row["Key1"]. "\t" . $row["Name"]. "\t" . $row["Hash"]. "";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
